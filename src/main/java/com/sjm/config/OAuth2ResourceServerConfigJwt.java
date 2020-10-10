6dfaec4cf1c137ed2124dd51af39e75c57fdabe2
yangjiho <yangjiho@cicd.null>
2020-10-11 01:54:07 +0900

    update Oauth Config

--- src/main/java/com/sjm/config/OAuth2ResourceServerConfigJwt.java
+++ src/main/java/com/sjm/config/OAuth2ResourceServerConfigJwt.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,56 +14,59 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import java.io.IOException;
+import io.micrometer.core.instrument.util.IOUtils;
+import org.springframework.core.io.Resource;
 
 @Configuration
 @EnableResourceServer
 public class OAuth2ResourceServerConfigJwt extends ResourceServerConfigurerAdapter {
 
-    @Autowired
-    private CustomAccessTokenConverter customAccessTokenConverter;
+	@Autowired
+	private CustomAccessTokenConverter customAccessTokenConverter;
 
-    @Override
-    public void configure(final HttpSecurity http) throws Exception {
-        // @formatter:off
-                http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                    .and()
-                    .authorizeRequests().anyRequest().permitAll();
-        // @formatter:on                
-    }
+	@Override
+	public void configure(final HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and().authorizeRequests()
+				.anyRequest().permitAll();
+		// @formatter:on
+	}
 
-    @Override
-    public void configure(final ResourceServerSecurityConfigurer config) {
-        config.tokenServices(tokenServices());
-    }
+	@Override
+	public void configure(final ResourceServerSecurityConfigurer config) {
+		config.tokenServices(tokenServices());
+	}
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
 
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setAccessTokenConverter(customAccessTokenConverter);
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
 
-        converter.setSigningKey("123");
-        // final Resource resource = new ClassPathResource("public.txt");
-        // String publicKey = null;
-        // try {
-        // publicKey = IOUtils.toString(resource.getInputStream());
-        // } catch (final IOException e) {
-        // throw new RuntimeException(e);
-        // }
-        // converter.setVerifierKey(publicKey);
-        return converter;
-    }
+		converter.setAccessTokenConverter(customAccessTokenConverter);
+		
+		final Resource resource = new ClassPathResource("public.txt");
+		String publicKey = null;
+		try {
+			publicKey = IOUtils.toString(resource.getInputStream());
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
+		converter.setVerifierKey(publicKey);
 
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        return defaultTokenServices;
-    }
+		return converter;
+	}
+
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		return defaultTokenServices;
+	}
 
 }
