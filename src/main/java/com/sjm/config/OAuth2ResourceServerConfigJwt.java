56cb5a75eb38ccd438842711f06cfd4a7304bb72
shinjaemin <shinjaemin@cicd.null>
2020-10-18 18:52:37 +0900

    update configuration

--- src/main/java/com/sjm/config/OAuth2ResourceServerConfigJwt.java
+++ src/main/java/com/sjm/config/OAuth2ResourceServerConfigJwt.java
@@ -1,9 +1,13 @@
 package com.sjm.config;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,6 +17,8 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import io.micrometer.core.instrument.util.IOUtils;
 
 @Configuration
 @EnableResourceServer
@@ -45,15 +51,25 @@ public class OAuth2ResourceServerConfigJwt extends ResourceServerConfigurerAdapt
         final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
         converter.setAccessTokenConverter(customAccessTokenConverter);
 
-        converter.setSigningKey("123");
-        // final Resource resource = new ClassPathResource("public.txt");
-        // String publicKey = null;
-        // try {
-        // publicKey = IOUtils.toString(resource.getInputStream());
-        // } catch (final IOException e) {
-        // throw new RuntimeException(e);
-        // }
-        // converter.setVerifierKey(publicKey);
+//        converter.setSigningKey("123");
+//		 final Resource resource = new ClassPathResource("public.txt");
+//		 String publicKey = null;
+//		 try {
+//		 publicKey = IOUtils.toString(resource.getInputStream());
+//		 } catch (final IOException e) {
+//		 throw new RuntimeException(e);
+//		 }
+//		 converter.setVerifierKey(publicKey);
+		 
+		 Resource resource = new ClassPathResource("publichy.txt");
+	        String publicKey = null;
+	        try {
+	            publicKey = IOUtils.toString(resource.getInputStream());
+	        } catch (final IOException e) {
+	            throw new RuntimeException(e);
+	        }
+	        converter.setVerifierKey(publicKey);
+	        
         return converter;
     }
 
