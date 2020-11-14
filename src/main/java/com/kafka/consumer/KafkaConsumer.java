6ebbb334f1d89785cf700d89b2521382eaa75da4
zhoang3 <root@ip-172-31-39-150.ap-northeast-2.compute.internal>
2020-11-14 13:42:05 +0000

    message

--- src/main/java/com/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/kafka/consumer/KafkaConsumer.java
@@ -4,9 +4,12 @@ import org.springframework.kafka.annotation.KafkaListener;
 
 public class KafkaConsumer {
 	
-	@KafkaListener(topics = "${message.topic.name}", groupId = "{kafka.groupId}")
+	@KafkaListener(topics = "test")
 	public String Listen(String message) {
 		String result = "Foo Received Message in group : " + message;
+		
+		System.out.println("Received Message");
+	
 		return result;
 	}
 	
