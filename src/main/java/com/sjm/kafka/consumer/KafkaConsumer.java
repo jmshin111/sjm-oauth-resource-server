6de4a0ad1309ddd7b826483ecfcc5ba69af8d51b
zhonag3 <wlgh7782@gmail.com>
2020-11-15 17:11:06 +0900

    update

--- src/main/java/com/sjm/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/sjm/kafka/consumer/KafkaConsumer.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -26,14 +27,14 @@ public class KafkaConsumer {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(KafkaConsumer.class);
 	
-	private RestTemplate restClient = null;
-	
 	@KafkaListener(topics = "${message.topic.name}", groupId = "${kafka.groupId}") 
 	public void listener(String message) {
 
 		String result = "Foo Received Message in group : " + message;
 	
 		LOG.info(result);
+		
+		RestTemplate restClient = new RestTemplate();
 		
 		HttpHeaders httpHeaders = new HttpHeaders();
 		
@@ -57,18 +58,17 @@ public class KafkaConsumer {
 				.build()
 				.toString();
 		
-		
 		// 2. RestTemplate를 이용해서 API를 요청한다.
 		HttpStatus respStatus = null;
 		String respBody = null;
 		
-		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+//		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
 		
 		try {
 			
 			// Call Rest API
 			/******************************************************************************************************/
-			requestLog(request.getRequestURI(), uri, method, reqEntity.getHeaders(), reqEntity.getBody());
+//			requestLog(request.getRequestURI(), uri, method, reqEntity.getHeaders(), reqEntity.getBody());
 //			ResponseEntity<String> respEntity = restClient.exchange(uri, method, reqEntity, String.class, uriVars);
 
 			ResponseEntity<String> respEntity = restClient.exchange(uri, method, reqEntity, String.class);
@@ -77,7 +77,7 @@ public class KafkaConsumer {
 			// 예외 없이 정상적으로 처리 된 경우
 			/******************************************************************************************************/
 			respStatus = respEntity.getStatusCode();
-			responseLog(request.getRequestURI(), respStatus, respEntity.getBody());
+//			responseLog(request.getRequestURI(), respStatus, respEntity.getBody());
 			/******************************************************************************************************/
 			
 			if (respEntity.hasBody()) {	// body 데이터가 존재하는 경우
@@ -86,45 +86,45 @@ public class KafkaConsumer {
 			
 		} catch (HttpClientErrorException | HttpServerErrorException e) {
 			respStatus = e.getStatusCode();
-			errorLog(request.getRequestURI(), respStatus, e);
+//			errorLog(request.getRequestURI(), respStatus, e);
 		} catch (UnknownHttpStatusCodeException e){
 			respStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-			errorLog(request.getRequestURI(), respStatus, e);
+//			errorLog(request.getRequestURI(), respStatus, e);
 		} catch (Exception e) {
 			respStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-			errorLog(request.getRequestURI(), respStatus, e);
+//			errorLog(request.getRequestURI(), respStatus, e);
 		}		
 		
 		
 	}
 	
-	private void requestLog(String uri, String liscoreUri, HttpMethod method, HttpHeaders headers, Object requestBody) {
-		LOG.info("[ LIS-CORE REST Request ]");
-		LOG.info("--------------------------------------------------------------------------------------------------------------------------");
-		LOG.info("request uri: {}", uri);
-		LOG.info("lis-core uri: {}", liscoreUri);
-		LOG.info("method: {}", method);
-		LOG.info("requestHeaders: {}", headers);
-		LOG.info("requestBody: {}", requestBody);
-		LOG.info("--------------------------------------------------------------------------------------------------------------------------");
-	}
-	
-	private void responseLog(String uri, HttpStatus respStatus, Object responseBody) {
-		LOG.info("[ LIS-CORE REST Response ]");
-		LOG.info("--------------------------------------------------------------------------------------------------------------------------");
-		LOG.info("request uri: {}", uri);
-		LOG.info("respStatus: {}", respStatus);
-		LOG.info("responseBody: {}", responseBody);
-		LOG.info("--------------------------------------------------------------------------------------------------------------------------");
-	}
-	
-	private void errorLog(String uri, HttpStatus respStatus, Exception e) {
-		LOG.error("[ LIS-CORE REST Response ERROR ]");
-		LOG.error("--------------------------------------------------------------------------------------------------------------------------");
-		LOG.error("request uri: {}", uri);
-		LOG.error("respStatus: {}", respStatus);
-		LOG.error("--------------------------------------------------------------------------------------------------------------------------");
-		LOG.error("Exception: ", e);
-	}
+//	private void requestLog(String uri, String liscoreUri, HttpMethod method, HttpHeaders headers, Object requestBody) {
+//		LOG.info("[ LIS-CORE REST Request ]");
+//		LOG.info("--------------------------------------------------------------------------------------------------------------------------");
+//		LOG.info("request uri: {}", uri);
+//		LOG.info("lis-core uri: {}", liscoreUri);
+//		LOG.info("method: {}", method);
+//		LOG.info("requestHeaders: {}", headers);
+//		LOG.info("requestBody: {}", requestBody);
+//		LOG.info("--------------------------------------------------------------------------------------------------------------------------");
+//	}
+//	
+//	private void responseLog(String uri, HttpStatus respStatus, Object responseBody) {
+//		LOG.info("[ LIS-CORE REST Response ]");
+//		LOG.info("--------------------------------------------------------------------------------------------------------------------------");
+//		LOG.info("request uri: {}", uri);
+//		LOG.info("respStatus: {}", respStatus);
+//		LOG.info("responseBody: {}", responseBody);
+//		LOG.info("--------------------------------------------------------------------------------------------------------------------------");
+//	}
+//	
+//	private void errorLog(String uri, HttpStatus respStatus, Exception e) {
+//		LOG.error("[ LIS-CORE REST Response ERROR ]");
+//		LOG.error("--------------------------------------------------------------------------------------------------------------------------");
+//		LOG.error("request uri: {}", uri);
+//		LOG.error("respStatus: {}", respStatus);
+//		LOG.error("--------------------------------------------------------------------------------------------------------------------------");
+//		LOG.error("Exception: ", e);
+//	}
 	
 }
