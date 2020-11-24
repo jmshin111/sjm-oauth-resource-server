a36cef92a8b65f3c937d6d331f346c76f40dd167
shinjaemin <shinjaemin@cicd.null>
2020-11-25 00:47:59 +0900

    aa

--- src/main/java/com/sjm/kafka/consumer/KafkaConsumerConfig.java
+++ src/main/java/com/sjm/kafka/consumer/KafkaConsumerConfig.java
@@ -4,22 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.Serde;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import com.sjm.web.dto.Result;
 
+@EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
 
@@ -38,22 +37,25 @@ public class KafkaConsumerConfig {
 	@Bean
 	public Map<String, Object> consumerConfigs() {
 		
-//		ObjectMapper mapper = new ObjectMapper();
-		Serde<Result> resultSerde = new JsonSerde<>(Result.class);
-
 		Map<String, Object>	props = new HashMap<>();
 		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, resultSerde.deserializer().getClass());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 		
 		return props;
 	}
 	
 	@Bean
 	public ConsumerFactory<String, Result> consumerFactory(){
-		return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+		
+		DefaultKafkaConsumerFactory<String, Result> defaultFactory = new DefaultKafkaConsumerFactory<>(consumerConfigs());
+		defaultFactory.setValueDeserializer(new JsonDeserializer<>(Result.class));
+		
+		return defaultFactory;
 	}
+	
+	
 	
 	/**
 	 * KafkaListenerContainer
@@ -64,6 +66,7 @@ public class KafkaConsumerConfig {
 	@Bean
 	public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Result>> kafkaListenerContainer(){
 		ConcurrentKafkaListenerContainerFactory<String, Result> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		
 		factory.setConsumerFactory(consumerFactory());
 		
 		return factory;
